3ae5455af58ce6476a7142483bb3e379879cb72c
Praful Chopade <p021097@gmail.com>
2026-04-01 23:20:26 +0530

    updated backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -3,7 +3,8 @@ import * as XLSX from "xlsx";
 
 export const StoreContext = createContext(null);
 
-const API_BASE_URL = "http://localhost:4000/api/files";
+// const API_BASE_URL = "http://localhost:4000/api/files";
+const API_BASE_URL = "https://proto-assignment-backend.onrender.com";
 
 const normalizeUrl = (value) => {
   if (!value) return null;
